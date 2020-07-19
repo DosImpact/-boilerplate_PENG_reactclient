ff286e99416c43a089b3b33e4ad27653f1761fdd
DosImpact <ypd03008@gmail.com>
2020-07-19 15:40:46 +0900

    Updates

---
+++ precache-manifest.4c4d9b14cd4495c065a751c8f1a9575b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc8544aa4d78b730c4526baa8e4f717",
+    "url": "/boilerplate_PENG_reactclient/index.html"
+  },
+  {
+    "revision": "5f546785169979a1ee32",
+    "url": "/boilerplate_PENG_reactclient/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "5f546785169979a1ee32",
+    "url": "/boilerplate_PENG_reactclient/static/js/2.23a5224b.chunk.js"
+  },
+  {
+    "revision": "843b1e6461bdc31a2f60595444704a94",
+    "url": "/boilerplate_PENG_reactclient/static/js/2.23a5224b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6364f3a4866ab31d3e21",
+    "url": "/boilerplate_PENG_reactclient/static/js/main.64706f8c.chunk.js"
+  },
+  {
+    "revision": "35f6e210adc251b19be5",
+    "url": "/boilerplate_PENG_reactclient/static/js/runtime-main.2184cc2b.js"
+  }
+]);
