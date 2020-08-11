c00e8b6a2a8dcd9ed7e73b268924229f963e3cf1
DosImpact <ypd03008@gmail.com>
2020-08-11 09:44:18 +0900

    formik test

--- src/test/Test.js
+++ src/test/Test.js
@@ -1,12 +1,53 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { logIn } from "_actions/log_actions";
+import * as React from "react";
+import { Formik, Field, Form } from "formik";
+import { TextField, Button, Checkbox, Radio } from "@material-ui/core";
 
-function Test() {
-  const dispatch = useDispatch();
-  const result = dispatch(logIn("Fake Toekn"));
-  console.log("result", result);
-  return <div>Tets.js</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <Formik
+        onSubmit={(data, { setSubmitting }) => {
+          setSubmitting(true);
+          console.log(data);
+          setSubmitting(false);
+        }}
+        initialValues={{
+          firstName: "dos",
+          lastName: "impact",
+          isTall: false,
+          foods: [],
+          aniaml: "",
+        }}
+      >
+        {({ values, isSubmitting }) => (
+          <Form>
+            <div>
+              <Field name="firstName" type="input" as={TextField} />
+            </div>
+            <div>
+              <Field name="lastName" type="input" as={TextField} />
+            </div>
+            <div>
+              <Button disabled={isSubmitting} type="submit">
+                submit
+              </Button>
+            </div>
+            <div>
+              <Field type="checkbox" name="isTall" as={Checkbox} />
+            </div>
+            <div>question 01 : </div>
+            <Field type="checkbox" value="01" name="foods" as={Checkbox} />
+            <Field type="checkbox" value="02" name="foods" as={Checkbox} />
+            <Field type="checkbox" value="03" name="foods" as={Checkbox} />
+            <div>question 02 : </div>
+            <Field type="radio" value="01" name="aniaml" as={Radio} />
+            <Field type="radio" value="02" name="aniaml" as={Radio} />
+            <Field type="radio" value="03" name="aniaml" as={Radio} />
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
-
-export default Test;
