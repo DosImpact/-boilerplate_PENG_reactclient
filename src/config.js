343685f1dbc6ad5b1343fac75629379e2d86f9fb
DosImpact <ypd03008@gmail.com>
2020-08-31 00:22:20 +0900

    change deploy Server

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  // SERVER_URI: "https://riely.herokuapp.com/",
-  SERVER_URI: "http://localhost:4000/",
+  SERVER_URI: "https://riely.herokuapp.com/",
+  // SERVER_URI: "http://localhost:4000/",
   //   SERVER_URI: "http://133.186.241.220:7000/",
 };
